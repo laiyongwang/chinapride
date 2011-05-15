e31b365c88a38376259b393dcd55ad2ba26c6d9f
Little Pan <littlepxy@gmail.com>
2011-05-15 21:31:06 +0800

    Little checkin 1st version of chinapride

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chinapride::Application.config.secret_token = '05d81be049b7f5b10ff94458226970c0b970109887a16c79c4836acd34ad4bf1a8132c602830528eb65cbd4f16360e5d8964fdfc64b7e9d59e1a0887e1417d85'
