35aa150b47a333c7cb37d428c9a364f4023933ed
Little Pan <littlepxy@gmail.com>
2011-06-12 14:39:53 +0800

    small change in s3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,14 @@
+    if Rails.env == "production" 
+      S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "ourbucket"} 
+    else 
+      S3_CREDENTIALS = Rails.root.join("#{RAILS_ROOT}/config/s3.yml") 
+    end
 class Product < ActiveRecord::Base
     has_attached_file :photo,
       :storage => :s3,
       :bucket => 'mybucket',
-      :s3_credentials => {
-        :access_key_id => ENV['AKIAJUN4UEWRI6OA4IQQ'],
-        :secret_access_key => ENV['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']
-      }
+      :s3_credentials => S3_CREDENTIALS
+      
     belongs_to :category
     
     
